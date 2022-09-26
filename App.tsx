274e6f0a6db43fa2f524da6284d37bad9c65e173
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2022-09-26 19:53:57 +0200

    adding paper

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import GraphQLClient from './src/api/graphql/GraphQLClient';
 import QueryClientWrap from './src/api/rest/QueryClientWrap';
 import Routes from './src/routes/Routes';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 const App = () => {
   return (
     <QueryClientWrap>
       <GraphQLClient>
-        <Routes />
+        <PaperProvider>
+          <Routes />
+        </PaperProvider>
       </GraphQLClient>
     </QueryClientWrap>
   );
