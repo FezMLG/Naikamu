36732a5c22fac3bcbc7c117a0b239456e4b537ce
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2022-09-28 16:51:21 +0200

    remove console logs

--- src/api/rest/frixy/getTitleDetails.ts
+++ src/api/rest/frixy/getTitleDetails.ts
@@ -2,7 +2,6 @@ import { RestClient } from '../RestClient';
 
 export const getTitleDetails = async (link: string) => {
   const baseURL = `https://frixysubs.pl/api/anime/${link}`;
-  console.log(baseURL);
   const response = await new RestClient(baseURL).get<any>();
   return response;
 };
--- src/api/rest/frixy/searchForTitle.ts
+++ src/api/rest/frixy/searchForTitle.ts
@@ -5,7 +5,6 @@ export const searchForTitle = async (title: string) => {
   const baseURL = `https://frixysubs.pl/api/anime?limit=15&offset=0&search=${encodeURI(
     title,
   )}`;
-  console.log(baseURL);
   const response = await new RestClient(baseURL).get<IFrixySearchResults>();
   return response;
 };
