321b1e55f93dd52b6d241184622721f503520c3e
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2022-09-30 23:17:35 +0200

    beta phase for infinite scroll

--- src/pages/BrowsePage.tsx
+++ src/pages/BrowsePage.tsx
@@ -1,45 +1,87 @@
-import { StyleSheet, ActivityIndicator, View, ScrollView } from 'react-native';
+import {
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+  Platform,
+  View,
+} from 'react-native';
 import React from 'react';
-import { useQuery } from '@apollo/client';
+import { NetworkStatus, useQuery } from '@apollo/client';
 import { LIST_OF_ANIME } from '../api/graphql/anilist/listOfAnime';
-import { IALListOfAnime } from '../interfaces';
+import { IALListOfAnime, Media } from '../interfaces';
 import { darkStyle } from '../styles/darkMode.style';
-import BrowseElement from './BrowseElement';
+import BrowseElement from '../components/browse/BrowseElement';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { maxWidth } from '../components/maxDimensions';
 
+const perPage = 25;
 const BrowsePage = ({ navigation }: any) => {
-  const { loading, data, error } = useQuery<IALListOfAnime>(LIST_OF_ANIME, {
-    variables: {
-      page: 1,
-      perPage: 25,
-    },
-  });
+  const { isTV } = Platform;
+  const { loading, data, error, fetchMore, refetch, networkStatus } =
+    useQuery<IALListOfAnime>(LIST_OF_ANIME, {
+      variables: {
+        page: 1,
+        perPage: perPage,
+      },
+      notifyOnNetworkStatusChange: true,
+    });
 
-  // if (loading) return null;
+  const handleOnEndReached = () => {
+    if (data?.Page.pageInfo.hasNextPage) {
+      console.log('has next page');
+      return fetchMore({
+        variables: {
+          page: data.Page.pageInfo.currentPage + 1,
+          perPage: perPage,
+        },
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          const newEntries = fetchMoreResult.Page.media;
+          return {
+            Page: {
+              pageInfo: fetchMoreResult.Page.pageInfo,
+              media: [...previousResult.Page.media, ...newEntries],
+            },
+          };
+        },
+      });
+    }
+  };
+
+  const refreshing = networkStatus === NetworkStatus.refetch;
+  // prevent the loading indicator from appearing while refreshing
+  if (loading && data?.Page.media.length === 0 && !refreshing) {
+    return (
+      <View>
+        <ActivityIndicator size="large" color="rgb(0, 122, 255)" />
+      </View>
+    );
+  }
+
+  const renderItem = ({ item }: { item: Media }) => (
+    <BrowseElement anime={item} navigation={navigation} />
+  );
+
   if (error) {
     console.log(error);
   }
 
   return (
     <SafeAreaView style={[styles.container, darkStyle.background]}>
-      {loading ? (
-        <ActivityIndicator size="large" />
-      ) : (
-        <ScrollView>
-          <View style={styles.autoGrid}>
-            {data &&
-              data.Page.media.map(item => {
-                return (
-                  <BrowseElement
-                    key={item.id}
-                    anime={item}
-                    navigation={navigation}
-                  />
-                );
-              })}
-          </View>
-        </ScrollView>
+      {data && (
+        <FlatList
+          data={data.Page.media}
+          renderItem={renderItem}
+          initialNumToRender={isTV ? 8 : 4}
+          numColumns={Math.floor(maxWidth() / 240)}
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyExtractor={() => Math.random().toString()}
+          onEndReachedThreshold={1}
+          onEndReached={handleOnEndReached}
+          onRefresh={refetch}
+          refreshing={refreshing}
+        />
       )}
+      {loading && <ActivityIndicator size="large" />}
     </SafeAreaView>
   );
 };
