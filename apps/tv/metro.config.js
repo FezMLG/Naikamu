8af78a5923005c8c4273a5b87d42631986098089
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2022-11-11 22:59:43 +0100

    update ios tv

--- apps/tv/metro.config.js
+++ apps/tv/metro.config.js
@@ -1,9 +1,11 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const path = require('path');
+
+const extraNodeModules = {
+  '@aniwatch/translations': path.resolve(
+    __dirname + '/../../lib/translations/',
+  ),
+};
+const watchFolders = [path.resolve(__dirname + '/../../lib/translations/')];
 
 module.exports = {
   transformer: {
@@ -14,4 +16,8 @@ module.exports = {
       },
     }),
   },
+  resolver: {
+    extraNodeModules,
+  },
+  watchFolders,
 };
