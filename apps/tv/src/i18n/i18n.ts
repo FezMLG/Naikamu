9de9864815636a3144e8213e4d616e02758d7c5f
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2022-11-12 23:46:19 +0100

    almost working android

--- apps/tv/src/i18n/i18n.ts
+++ apps/tv/src/i18n/i18n.ts
@@ -1,8 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import * as RNLocalize from 'react-native-localize';
-import pl from '../../../../translations/pl/pl.json';
-import en from '../../../../translations/pl/pl.json';
+import { pl, en } from '@aniwatch/translations';
 
 const resources = {
   pl: {
