293f53b55f25317abd029cd66727ca0e02d80d1d
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2022-11-12 13:24:28 +0100

    update to tv

--- apps/tv/android/app/src/main/java/com/aniwatchtv/MainActivity.java
+++ apps/tv/android/app/src/main/java/com/aniwatchtv/MainActivity.java
@@ -1,12 +1,16 @@
-package com.aniwatch;
+import android.os.Bundle;
+package com.aniwatchtv;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import android.os.Bundle;
-import androidx.annotation.Nullable;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -14,7 +18,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "AniWatch";
+    return "AniWatchTV";
   }
 
   /**
@@ -25,11 +29,6 @@ public class MainActivity extends ReactActivity {
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new MainActivityDelegate(this, getMainComponentName());
-  }
-
-  @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
   }
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
---
+++ apps/tv/metro.config.js
@@ -0,0 +1,30 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const path = require('path');
+
+const extraNodeModules = {
+  '@aniwatch/translations': path.resolve(
+    __dirname + '/../../lib/translations/',
+  ),
+};
+const watchFolders = [path.resolve(__dirname + '/../../lib/translations/')];
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    extraNodeModules,
+  },
+  watchFolders,
+};
