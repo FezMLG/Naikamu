90b102c23734012e6bad21098a217b79ef17bdaa
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2022-11-13 23:55:52 +0100

    working barebone

--- apps/tv/src/enums/action-type.enum.ts
+++
@@ -1,3 +0,0 @@
-export enum ActionType {
-  AccountDelete = 'AccountDelete',
-}
--- apps/tv/src/interfaces/user.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  displayName: string | null;
-  email: string | null;
-  emailVerified: boolean;
-  isAnonymous: boolean;
-  uid: string;
-  picture: string | null;
-}
