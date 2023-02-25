e70ace7500959a2593fa07e32c1d7eb932114390
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2023-02-26 00:38:48 +0100

    return to start

--- apps/mobile/src/screens/BrowseScreen.tsx
+++ apps/mobile/src/screens/BrowseScreen.tsx
@@ -21,28 +21,12 @@ const BrowseScreen = ({ navigation }: BrowseScreenProps) => {
   const [contentVerticalOffset, setContentVerticalOffset] = useState(0);
   const listRef = useRef<FlatList>(null);
 
-  const queryClient = useQueryClient();
-  const initialData = () =>
-    queryClient.getQueryState<AnimeList>(['browse', season, year])?.data;
   const { isLoading, data, refetch, fetchNextPage, isRefetching } =
     useInfiniteQuery<AnimeList>(
       ['browse', season, year],
-      ({
-        pageParam = initialData()
-          ? initialData()!.Page.pageInfo.currentPage
-          : 0,
-      }) =>
+      ({ pageParam }) =>
         apiClient.getAnimeList({ page: pageParam, season, seasonYear: year }),
       {
-        initialData: () => {
-          const temp = initialData();
-          if (temp) {
-            return {
-              pageParams: [undefined],
-              pages: [temp],
-            };
-          }
-        },
         getNextPageParam: lastPage => lastPage.Page.pageInfo.currentPage + 1,
       },
     );
