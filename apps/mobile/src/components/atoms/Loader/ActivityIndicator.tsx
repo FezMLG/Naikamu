ff09af1740086ddd6ce4a09941dbb3a4567dc93d
Konrad Chwesiuk <konradchw@gmail.com>
2023-05-25 21:42:07 +0200

    feat: change indicator on loading

--- apps/mobile/src/components/atoms/Loader/ActivityIndicator.tsx
+++ apps/mobile/src/components/atoms/Loader/ActivityIndicator.tsx
@@ -3,6 +3,12 @@ import { ActivityIndicator as PIA } from 'react-native-paper';
 
 import { colors } from '../../../styles';
 
-export const ActivityIndicator = ({ visible }: { visible: boolean }) => {
-  return <PIA animating={visible} color={colors.accent.color} />;
+export const ActivityIndicator = ({
+  visible,
+  size = 'small',
+}: {
+  visible: boolean;
+  size: 'small' | 'large';
+}) => {
+  return <PIA size={size} animating={visible} color={colors.accent.color} />;
 };
