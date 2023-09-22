fada9bb44bb81574ce609e6d13bd2fccd2fd17f8
Konrad Chwesiuk <konradchw@gmail.com>
2023-09-22 14:39:25 +0200

    feat: upgrade to react native 0.72.4

--- apps/mobile/ios/AniWatch/AppDelegate.mm
+++ apps/mobile/ios/AniWatch/AppDelegate.mm
@@ -34,16 +34,6 @@
 #endif
 }
 
-/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
-///
-/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
-/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
-/// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
-- (BOOL)concurrentRootEnabled
-{
-  return true;
-}
-
 - (void)application:(UIApplication *)application handleEventsForBackgroundURLSession:(NSString *)identifier completionHandler:(void (^)())completionHandler
 {
   [RNFSManager setCompletionHandlerForIdentifier:identifier completionHandler:completionHandler];
