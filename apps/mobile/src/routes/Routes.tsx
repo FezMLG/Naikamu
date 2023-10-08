4140302b117f1cc68862d89b33a0a9cb9feff94a
Konrad Chwesiuk <konradchw@gmail.com>
2023-10-08 12:37:45 +0200

    feat: add screen tracking

--- apps/mobile/src/routes/Routes.tsx
+++ apps/mobile/src/routes/Routes.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import analytics from '@react-native-firebase/analytics';
 import { NavigationContainer, Theme } from '@react-navigation/native';
 
 import { SplashScreen } from '../screens';
@@ -23,12 +24,30 @@ const linking = {
 };
 
 function Routes({ theme }: { theme: Theme }) {
+  const routeNameRef = React.useRef<any>();
+  const navigationRef = React.useRef<any>();
   const user = useUserStore(state => state.user);
 
   return (
     <NavigationContainer
       fallback={<SplashScreen />}
       linking={linking}
+      onReady={() => {
+        routeNameRef.current = navigationRef.current?.getCurrentRoute().name;
+      }}
+      onStateChange={async () => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = navigationRef.current?.getCurrentRoute().name;
+
+        if (previousRouteName !== currentRouteName) {
+          await analytics().logScreenView({
+            screen_name: currentRouteName,
+            screen_class: currentRouteName,
+          });
+        }
+        routeNameRef.current = currentRouteName;
+      }}
+      ref={navigationRef}
       theme={theme}>
       {user && user?.emailVerified ? <RootStack /> : <AuthStack />}
     </NavigationContainer>
