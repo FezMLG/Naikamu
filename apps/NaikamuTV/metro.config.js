e492629a009413f905439a2758f8b3f4a4ffb17c
Konrad Chwesiuk <konradchw@gmail.com>
2023-10-22 12:41:03 +0200

    feat: add lint and navigation

--- apps/NaikamuTV/metro.config.js
+++ apps/NaikamuTV/metro.config.js
@@ -1,4 +1,5 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,11 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [
+    path.resolve(__dirname + '/../../lib/translations/'),
+    path.resolve(__dirname + '/../../lib/shared/'),
+  ],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
