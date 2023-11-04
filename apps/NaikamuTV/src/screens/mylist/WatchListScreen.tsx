3469021744c7b2029a256ba20f488e26e8bb7dcb
Konrad Chwesiuk <konradchw@gmail.com>
2023-11-05 00:22:13 +0100

    feat: show after focus

--- apps/NaikamuTV/src/screens/mylist/WatchListScreen.tsx
+++ apps/NaikamuTV/src/screens/mylist/WatchListScreen.tsx
@@ -54,75 +54,81 @@ export const WatchListScreen = ({}: MyListStackWatchListScreenProps) => {
           flexDirection: 'row',
           flex: 1,
         }}>
-        <View
-          style={{
-            gap: 15,
-            width: '60%',
-            flex: 1,
-            zIndex: 10,
-            marginLeft: 10,
-            marginTop: 10,
-          }}>
-          {/*<Button*/}
-          {/*  label={translate('auth.logout')}*/}
-          {/*  onPress={() => userService.logoutUser()}*/}
-          {/*  type="secondary"*/}
-          {/*/>*/}
-          <View>
-            <SeriesDetails.Title
-              romaji={selectedSeries?.title}
-              styles={{
-                width: '100%',
-              }}
-            />
-            <Text style={[colors.textLight, fontStyles.normal]}>
-              project No. 9
-            </Text>
-          </View>
-          <View
-            style={{
-              flexDirection: 'row',
-              gap: 20,
-            }}>
-            <Text style={[colors.textLight, fontStyles.normal]}>
-              Popularność: 89%
-            </Text>
-            <Text style={[colors.textLight, fontStyles.normal]}>Zima 2022</Text>
-            <Text style={[colors.textLight, fontStyles.normal]}>
-              {selectedSeries?.id}
-            </Text>
-          </View>
-          <SeriesDetails.Genres
-            genres={['Thriller', 'Action', 'Ecchi', 'Harem']}
-          />
-        </View>
-        <View
-          style={{
-            flex: 1,
-            position: 'absolute',
-            right: 0,
-            width: '65%',
-            height: '100%',
-          }}>
-          <ProgressiveImage
-            resizeMode="cover"
-            source={selectedSeries?.poster ?? ''}
-            style={{
-              zIndex: 1,
-              width: '100%',
-              height: '100%',
-            }}
-          />
-          <View
-            style={{
-              zIndex: 5,
-              backgroundColor: 'rgba(0,0,0,0.6)',
-              width: '30%',
-              height: '100%',
-              position: 'absolute',
-            }}
-          />
-        </View>
+        {selectedSeries ? (
+          <>
+            <View
+              style={{
+                gap: 15,
+                width: '60%',
+                flex: 1,
+                zIndex: 10,
+                marginLeft: 10,
+                marginTop: 10,
+              }}>
+              {/*<Button*/}
+              {/*  label={translate('auth.logout')}*/}
+              {/*  onPress={() => userService.logoutUser()}*/}
+              {/*  type="secondary"*/}
+              {/*/>*/}
+              <View>
+                <SeriesDetails.Title
+                  romaji={selectedSeries?.title}
+                  styles={{
+                    width: '100%',
+                  }}
+                />
+                <Text style={[colors.textLight, fontStyles.normal]}>
+                  project No. 9
+                </Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  gap: 20,
+                }}>
+                <Text style={[colors.textLight, fontStyles.normal]}>
+                  Popularność: 89%
+                </Text>
+                <Text style={[colors.textLight, fontStyles.normal]}>
+                  Zima 2022
+                </Text>
+                <Text style={[colors.textLight, fontStyles.normal]}>
+                  {selectedSeries?.id}
+                </Text>
+              </View>
+              <SeriesDetails.Genres
+                genres={['Thriller', 'Action', 'Ecchi', 'Harem']}
+              />
+            </View>
+            <View
+              style={{
+                flex: 1,
+                position: 'absolute',
+                right: 0,
+                width: '65%',
+                height: '100%',
+              }}>
+              <ProgressiveImage
+                resizeMode="cover"
+                source={selectedSeries?.poster ?? ''}
+                style={{
+                  zIndex: 1,
+                  width: '100%',
+                  height: '100%',
+                }}
+              />
+              <View
+                style={{
+                  zIndex: 5,
+                  backgroundColor: 'rgba(0,0,0,0.6)',
+                  width: '30%',
+                  height: '100%',
+                  position: 'absolute',
+                }}
+              />
+            </View>
+          </>
+        ) : null}
       </View>
       <View style={{ height: '55%' }}>
         <Text
