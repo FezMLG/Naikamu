0378d1413fdacb03b7787686fe7b5ab130a4c42f
Konrad Chwesiuk <konradchw@gmail.com>
2023-11-08 23:28:11 +0100

    feat: episode navigation with scroll to

--- apps/NaikamuTV/src/screens/series/EpisodesListScreen.tsx
+++ apps/NaikamuTV/src/screens/series/EpisodesListScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 import { AnimeEpisode } from '@naikamu/shared';
 import {
@@ -7,6 +7,8 @@ import {
   Image,
   SafeAreaView,
   Text,
+  View,
+  Pressable,
 } from 'react-native';
 
 import { useQuerySeriesEpisodes } from '../../api/hooks';
@@ -14,10 +16,71 @@ import { Episode, PageLayout } from '../../components';
 import { useTranslate } from '../../i18n/useTranslate';
 import { SeriesStackEpisodeScreenProps } from '../../routes';
 import { useSelectedSeriesStore } from '../../services';
-import { darkStyle, globalStyle } from '../../styles';
+import {
+  colors,
+  DarkColor,
+  darkStyle,
+  defaultRadius,
+  fontStyles,
+  globalStyle,
+} from '../../styles';
+
+function sliceIntoChunks<T>(array: T[], chunkSize = 10) {
+  const result: T[][] = [];
+
+  for (let index = 0; index < array.length; index += chunkSize) {
+    const chunk = array.slice(index, index + chunkSize);
+
+    result.push(chunk);
+  }
+
+  return result;
+}
+
+export const EpisodeNumber = ({
+  items,
+  onPress,
+}: {
+  items: AnimeEpisode[];
+  onPress: () => void;
+}) => {
+  const [isFocus, setIsFocus] = useState(false);
+
+  return (
+    <Pressable
+      onBlur={() => setIsFocus(() => false)}
+      onFocus={() => {
+        setIsFocus(() => true);
+      }}
+      onPress={onPress}
+      style={[
+        {
+          justifyContent: 'center',
+          width: '100%',
+          height: 40,
+          maxWidth: 200,
+          borderWidth: 1,
+          borderStyle: 'solid',
+          borderColor: 'blue',
+          backgroundColor: DarkColor.C800,
+          borderRadius: defaultRadius,
+          paddingHorizontal: 10,
+          marginTop: 15,
+        },
+        isFocus
+          ? { borderColor: colors.accent.color }
+          : { borderColor: 'transparent' },
+      ]}>
+      <Text style={[fontStyles.text, colors.textLight]}>
+        {items.at(0)?.number} - {items.at(-1)?.number}
+      </Text>
+    </Pressable>
+  );
+};
 
 export function EpisodesListScreen({ route }: SeriesStackEpisodeScreenProps) {
   const series = useSelectedSeriesStore(store => store.series);
+  const scrollViewRef = useRef<ScrollView>(null);
 
   const { translate } = useTranslate();
   const {
@@ -31,31 +94,58 @@ export function EpisodesListScreen({ route }: SeriesStackEpisodeScreenProps) {
     <SafeAreaView style={{ flex: 1 }}>
       <PageLayout.Loading isLoading={isLoading} />
       <PageLayout.Error isError={isError} refetch={refetch} />
-      <ScrollView style={styles.scrollView}>
-        <Image
-          resizeMode="contain"
-          source={require('../../assets/logo_docchi.png')}
-          style={[styles.logo]}
-        />
-        {episodes
-          ? episodes.episodes.map((episode: AnimeEpisode, index: number) => (
-              <Episode
-                episode={episode}
-                isWatched={episode.isWatched}
-                key={index}
-              />
-            ))
-          : null}
-        <Text style={[globalStyle.disclaimer, darkStyle.font]}>
-          {translate('anime_episodes.disclaimer')}
-        </Text>
-      </ScrollView>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+        }}>
+        <ScrollView
+          style={{
+            width: '30%',
+          }}>
+          <Text>Episodes</Text>
+          {episodes
+            ? sliceIntoChunks(episodes.episodes).map((item, index) => (
+                <EpisodeNumber
+                  items={item}
+                  key={index}
+                  onPress={() => {
+                    scrollViewRef.current?.scrollTo({
+                      y: ((item.at(0)?.number ?? 1) - 1) * 190,
+                      animated: true,
+                    });
+                  }}
+                />
+              ))
+            : null}
+        </ScrollView>
+        <ScrollView ref={scrollViewRef} style={styles.scrollView}>
+          <Image
+            resizeMode="contain"
+            source={require('../../assets/logo_docchi.png')}
+            style={[styles.logo]}
+          />
+          {episodes
+            ? episodes.episodes.map((episode: AnimeEpisode, index: number) => (
+                <Episode
+                  episode={episode}
+                  isWatched={episode.isWatched}
+                  key={index}
+                />
+              ))
+            : null}
+          <Text style={[globalStyle.disclaimer, darkStyle.font]}>
+            {translate('anime_episodes.disclaimer')}
+          </Text>
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   scrollView: {
+    width: '70%',
     marginHorizontal: 10,
   },
   logo: {
