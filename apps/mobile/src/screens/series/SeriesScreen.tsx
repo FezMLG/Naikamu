769d8aede48d87482d49f2703f188f286d916623
Konrad Chwesiuk <konradchw@gmail.com>
2023-12-10 11:47:38 +0100

    feat: add paralax effect for poster

--- apps/mobile/src/screens/series/SeriesScreen.tsx
+++ apps/mobile/src/screens/series/SeriesScreen.tsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
 
 import { BlurView } from '@react-native-community/blur';
-import { StyleSheet, ScrollView, View, Pressable } from 'react-native';
+import { StyleSheet, View, Pressable, Platform } from 'react-native';
+import Animated, {
+  interpolate,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { useQuerySeriesDetails } from '../../api/hooks';
@@ -12,24 +18,46 @@ import {
   WatchListStatusSelect,
   ProgressiveImage,
   PageLayout,
-  useLayout,
   PlatformExplicit,
 } from '../../components';
 import { SeriesStackSeriesScreenProps } from '../../routes';
 import { globalStyle, DarkColor, colors } from '../../styles';
-import { debugBorder } from '../../utils/debugBorder';
 
 export function SeriesScreen({
   route,
   navigation,
 }: SeriesStackSeriesScreenProps) {
   const { id } = route.params;
-  const layout = useLayout();
   const { data, isError, isLoading, refetch } = useQuerySeriesDetails(id);
   const [widthForStatus, setWidthForStatus] = useState(500);
 
+  const scrollPosition = useSharedValue(0);
+  const handleScroll = useAnimatedScrollHandler({
+    onScroll: event => {
+      scrollPosition.value = event.contentOffset.y;
+    },
+  });
+
+  const imageHeight = 300;
+  const index = 0;
+  const imageTranslate = useAnimatedStyle(() => {
+    const translateY = interpolate(
+      scrollPosition.value,
+      [
+        ((index - 1) * imageHeight) / 2,
+        (index * imageHeight) / 2,
+        ((index + 1) * imageHeight) / 2,
+      ],
+      [-100, 0, 100],
+    );
+
+    return {
+      transform: [{ translateY }],
+    };
+  }, []);
+
   return (
-    <PageLayout.Default margin={false} {...layout}>
+    <Animated.ScrollView onScroll={handleScroll} scrollEventThrottle={16}>
       <PlatformExplicit availablePlatforms={['android']}>
         <Pressable onPress={() => navigation.goBack()} style={styles.closeIcon}>
           <Icon name="close" size={20} style={colors.textLight} />
@@ -39,82 +67,83 @@ export function SeriesScreen({
       <PageLayout.Error isError={isError} refetch={refetch} />
       {data ? (
         <>
-          <PlatformExplicit availablePlatforms={['ios']}>
-            <ProgressiveImage
-              key="blurryImage"
-              source={data.coverImage.medium}
-              style={StyleSheet.absoluteFill}
+          <SeriesDetails.Poster
+            altImage={data.coverImage.extraLarge}
+            animationStyle={imageTranslate}
+            bannerImage={data.coverImage.extraLarge}
+          />
+          <View style={styles.body}>
+            <PlatformExplicit availablePlatforms={['ios']}>
+              <ProgressiveImage
+                key="blurryImage"
+                source={data.coverImage.medium}
+                style={StyleSheet.absoluteFill}
+              />
+              <BlurView
+                blurAmount={75}
+                blurType="dark"
+                reducedTransparencyFallbackColor={DarkColor.C900}
+                style={[StyleSheet.absoluteFill]}
+              />
+            </PlatformExplicit>
+            <SeriesDetails.Title
+              english={data.title.english}
+              romaji={data.title.romaji}
             />
-            <BlurView
-              blurAmount={75}
-              blurType="dark"
-              reducedTransparencyFallbackColor={DarkColor.C900}
-              style={[StyleSheet.absoluteFill]}
+            <SeriesDetails.SubTitle
+              english={data.title.english}
+              romaji={data.title.romaji}
             />
-          </PlatformExplicit>
-          <ScrollView style={styles.scrollView}>
-            <SeriesDetails.Poster
-              altImage={data.coverImage.extraLarge}
-              bannerImage={data.coverImage.extraLarge}
+            <View style={globalStyle.marginTop} />
+            <SeriesDetails.QuickInfoContainer data={data} />
+            {data.nextAiringEpisode ? (
+              <View style={globalStyle.marginTop} />
+            ) : null}
+            <SeriesDetails.NextEpisode
+              airingAt={data.nextAiringEpisode?.airingAt}
+              episode={data.nextAiringEpisode?.episode}
             />
-            <View style={styles.body}>
-              <SeriesDetails.Title
-                english={data.title.english}
-                romaji={data.title.romaji}
+            <View style={globalStyle.marginTop} />
+            <EpisodesButton series={data} />
+            <View style={globalStyle.marginTop} />
+            <View
+              onLayout={event =>
+                setWidthForStatus(event.nativeEvent.layout.width)
+              }
+              style={styles.watchlistTrailerContainer}>
+              <WatchListStatusSelect
+                initialWatchStatus={data.watchStatus}
+                parentWidth={widthForStatus}
+                seriesId={data.id}
               />
-              <SeriesDetails.SubTitle
-                english={data.title.english}
-                romaji={data.title.romaji}
-              />
-              <View style={globalStyle.marginTop} />
-              <SeriesDetails.QuickInfoContainer data={data} />
-              {data.nextAiringEpisode ? (
-                <View style={globalStyle.marginTop} />
-              ) : null}
-              <SeriesDetails.NextEpisode
-                airingAt={data.nextAiringEpisode?.airingAt}
-                episode={data.nextAiringEpisode?.episode}
-              />
-              <View style={globalStyle.marginTop} />
-              <EpisodesButton series={data} />
-              <View style={globalStyle.marginTop} />
-              <View
-                onLayout={event =>
-                  setWidthForStatus(event.nativeEvent.layout.width)
-                }
-                style={styles.watchlistTrailerContainer}>
-                <WatchListStatusSelect
-                  initialWatchStatus={data.watchStatus}
-                  parentWidth={widthForStatus}
-                  seriesId={data.id}
-                />
-                <SeriesDetails.Trailer trailer={data.trailer} />
-              </View>
-              <View style={globalStyle.marginTop} />
-              <SeriesDetails.Genres
-                color={data.coverImage.color}
-                genres={data.genres}
-              />
-              <View style={globalStyle.marginTopSmall} />
-              <SeriesDetails.AverageScore averageScore={data.averageScore} />
-              <View style={globalStyle.marginTop} />
-              <SeriesDetails.Description description={data.description} />
-              <View style={globalStyle.marginTop} />
-              <SeriesDetailsRelations relations={data.relations} />
-              <View style={globalStyle.marginTop} />
-              <SeriesDetails.DataSource sourceId={data.sourceId} />
+              <SeriesDetails.Trailer trailer={data.trailer} />
             </View>
-          </ScrollView>
+            <View style={globalStyle.marginTop} />
+            <SeriesDetails.Genres
+              color={data.coverImage.color}
+              genres={data.genres}
+            />
+            <View style={globalStyle.marginTopSmall} />
+            <SeriesDetails.AverageScore averageScore={data.averageScore} />
+            <View style={globalStyle.marginTop} />
+            <SeriesDetails.Description description={data.description} />
+            <View style={globalStyle.marginTop} />
+            <SeriesDetailsRelations relations={data.relations} />
+            <View style={globalStyle.marginTop} />
+            <SeriesDetails.DataSource sourceId={data.sourceId} />
+          </View>
         </>
       ) : null}
-    </PageLayout.Default>
+    </Animated.ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  scrollView: {},
   body: {
     paddingHorizontal: 16,
+    backgroundColor:
+      Platform.OS === 'ios' ? 'transparent' : colors.background.color,
+    paddingBottom: 16,
   },
   watchlistTrailerContainer: {
     flexDirection: 'row',
