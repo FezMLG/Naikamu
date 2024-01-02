325f9e4abe3c77b18406420df2f7ecb9422015de
Konrad Chwesiuk <konradchw@gmail.com>
2024-01-02 13:26:53 +0100

    feat: set fullscreen on webview

--- apps/mobile/src/screens/player/WebViewPlayerScreen.tsx
+++ apps/mobile/src/screens/player/WebViewPlayerScreen.tsx
@@ -1,6 +1,7 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { Pressable, View } from 'react-native';
+import SystemNavigationBar from 'react-native-system-navigation-bar';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { WebView } from 'react-native-webview';
 
@@ -14,6 +15,10 @@ export function WebViewPlayerScreen({
   const { uri } = route.params;
   const [isReloaded, setIsReloaded] = useState(false);
   const webViewRef = useRef<WebView>(null);
+
+  useEffect(() => {
+    SystemNavigationBar.fullScreen(true);
+  }, []);
 
   return (
     <View style={{ flex: 1, backgroundColor: colors.background.color }}>
@@ -39,7 +44,7 @@ export function WebViewPlayerScreen({
         style={{
           backgroundColor: colors.background.color,
           position: 'absolute',
-          bottom: 0,
+          top: 0,
           right: 0,
           width: 45,
           height: 45,
