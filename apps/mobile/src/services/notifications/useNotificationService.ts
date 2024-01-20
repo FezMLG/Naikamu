7dbe642f833dfd74718b0b3af38ef0c548e0317d
Konrad Chwesiuk <konradchw@gmail.com>
2024-01-20 22:35:12 +0100

    feat: prepare for long lasting

--- apps/mobile/src/services/notifications/useNotificationService.ts
+++ apps/mobile/src/services/notifications/useNotificationService.ts
@@ -1,4 +1,8 @@
-import { AndroidChannel, default as notifee } from '@notifee/react-native';
+import {
+  AndroidChannel,
+  default as notifee,
+  Notification,
+} from '@notifee/react-native';
 
 import { useTranslate } from '../../i18n/useTranslate';
 
@@ -24,17 +28,37 @@ export function useNotificationService() {
   const displayNotification = async (
     channelId: NotificationChannels,
     translationActionKey: string,
+    notification: Notification = {},
   ) => {
     await notifee.displayNotification({
+      ...notification,
       title: translate(
         `notification.${channelId}.${translationActionKey}.title`,
       ),
       body: translate(`notification.${channelId}.${translationActionKey}.body`),
       android: {
+        ...notification.android,
         channelId,
         pressAction: {
           id: 'default',
         },
+      },
+    });
+  };
+
+  const registerForegroundService = () => {
+    notifee.registerForegroundService(notification => {
+      return new Promise(() => {});
+    });
+  };
+
+  const attachNotificationToService = async (
+    channelId: NotificationChannels,
+    translationActionKey: string,
+  ) => {
+    await displayNotification(channelId, translationActionKey, {
+      android: {
+        asForegroundService: true,
       },
     });
   };
