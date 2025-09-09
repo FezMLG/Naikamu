041bdc50d3668366f30c7ea1479187d9b439296d
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2025-09-10 00:06:37 +0200

    feat: cleanup

--- apps/mobile/src/utils/logger.ts
+++ apps/mobile/src/utils/logger.ts
@@ -1,24 +1,31 @@
 import * as RNFS from '@dr.pogodin/react-native-fs';
 import pino from 'pino';
 
+const logsFilePrefix = 'logs';
+const logsFileExtension = 'json';
+const logsPath = RNFS.DocumentDirectoryPath + '/logs';
+
 const getLogFilePath = () => {
   const today = new Date();
   const yyyy = today.getFullYear();
   const mm = String(today.getMonth() + 1).padStart(2, '0');
   const dd = String(today.getDate()).padStart(2, '0');
 
-  return `${RNFS.DocumentDirectoryPath}/logs-${yyyy}-${mm}-${dd}.txt`;
+  return `${logsPath}/${logsFilePrefix}-${yyyy}-${mm}-${dd}.${logsFileExtension}`;
 };
 
 const cleanupOldLogs = async () => {
   try {
     const FILE_LIFE_TIME_IN_DAYS = 7;
-    const files = await RNFS.readDir(RNFS.DocumentDirectoryPath);
+    const files = await RNFS.readDir(logsPath);
     const now = Date.now();
     const fileLifeTimeInMs = FILE_LIFE_TIME_IN_DAYS * 24 * 60 * 60 * 1000;
 
     for (const file of files) {
-      if (file.name.startsWith('logs-') && file.name.endsWith('.txt')) {
+      if (
+        file.name.startsWith(`${logsFilePrefix}-`) &&
+        file.name.endsWith(`.${logsFileExtension}`)
+      ) {
         const age = now - file.mtime!.getTime();
 
         if (age > fileLifeTimeInMs) {
