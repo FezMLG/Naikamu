d6746654fb9fbcfce2fc45094442d8785676bf0a
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2025-11-30 14:08:46 +0100

    feat: android works

--- apps/mobile/android/app/src/main/java/com/tltcode/naikamu/MainApplication.kt
+++ apps/mobile/android/app/src/main/java/com/tltcode/naikamu/MainApplication.kt
@@ -1,5 +1,8 @@
-package com.tltworld.naikamu
+package com.tltcode.naikamu
 
+import android.content.res.Configuration
+import expo.modules.ApplicationLifecycleDispatcher
+import expo.modules.ReactNativeHostWrapper
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
