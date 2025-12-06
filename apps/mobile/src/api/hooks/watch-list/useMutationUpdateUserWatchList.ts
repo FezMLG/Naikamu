edad1273aff5526351f1c52869db5ece68d34eb6
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2025-12-06 17:03:55 +0100

    fix: import query client

--- apps/mobile/src/api/hooks/watch-list/useMutationUpdateUserWatchList.ts
+++ apps/mobile/src/api/hooks/watch-list/useMutationUpdateUserWatchList.ts
@@ -3,13 +3,12 @@ import { useState } from 'react';
 import { WatchStatus } from '@naikamu/shared';
 import { useMutation } from '@tanstack/react-query';
 
-import { APIClient } from '../../APIClient';
+import { apiClient } from '../../APIClient';
 
 export const useMutationUpdateUserWatchList = (
   watchStatus: WatchStatus,
   seriesId: string,
 ) => {
-  const apiClient = new APIClient();
   const [watching, setWatching] = useState<WatchStatus>(watchStatus);
   const mutation = useMutation({
     mutationFn: async () => {
